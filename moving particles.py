c1497d14b7e862b80bae6a89f71f76f2abeffd51
Edwin-Cherian <edwin.cherian@yahoo.co.uk>
2021-05-05 00:09:58 +0100

    Naive collision detection
    Checks every point pairwise to see if a collision has occurred. Very inefficient as time complexity is O(n^2). 1k particles barely works at 3fps

--- moving particles.py
+++ moving particles.py
@@ -19,30 +19,52 @@ pygame.init()
 pygame.display.set_caption("Moving particles")
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
-fps = 60
+fps = 600
 
 class Particle:
-    def __init__(self, x=0, y=0, xvel=0, yvel=0, acc=0, size=10):
+    def __init__(self, x=0, y=0, xvel=0, yvel=0, acc=0, size=3):
         self.x = random.randint(50,700)
         self.y = random.randint(50,700)
-        self.xvel = random.randint(-10,10)
-        self.yvel = random.randint(-10,10)
+        self.xvel = random.randint(-1,1)
+        self.yvel = random.randint(-1,1)
         self.acc = acc
         self.size = size
 
-    def move(self):
+
+    def checkcollision(self, particles):
+        for particle in particles:
+            if not (self.x+self.size<particle.x-particle.size or self.x-self.size>particle.x+particle.size or
+                self.y+self.size<particle.y-particle.size or self.y-self.size>particle.y+particle.size):
+                if particle != self:
+                    self.highlight()
+
+
+    def draw(self):
+        pygame.draw.circle(screen, BLACK, (self.x,self.y), self.size)
+
+
+    def highlight(self):
+        pygame.draw.circle(screen, RED, (self.x,self.y), self.size)
+
+
+    def move(self, particles):
         self.x += self.xvel
         self.y += self.yvel
         if self.x-self.size < 0 or self.x+self.size > WIDTH:
             self.xvel *= -1
 
+
         if self.y-self.size<0 or self.y+self.size>HEIGHT:
             self.yvel *= -1
 
+        self.draw()
+        self.checkcollision(particles)
 
 
 
-particles = [Particle() for i in range(5)]
+
+
+particles = [Particle() for i in range(100)]
 print(particles[0].__dict__)
 
 #<----RUN PROGRAM IN PYGAME---->
@@ -59,7 +81,6 @@ while run:
                 pass
     s=time.perf_counter()
     for particle in particles:
-        pygame.draw.circle(screen, BLACK, (particle.x, particle.y), particle.size)
-        particle.move()
-    #print(1/(time.perf_counter()-s))
+        particle.move(particles)
+    print(1/(time.perf_counter()-s))
     pygame.display.update()
