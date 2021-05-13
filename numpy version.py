c6ae15823cefb5428b0088e471e0f7fe8f4fe3a6
Edwin-Cherian <83539398+Edwin-Cherian@users.noreply.github.com>
2021-05-13 11:39:26 +0100

    Commented the code

--- numpy version.py
+++ numpy version.py
@@ -20,7 +20,7 @@ pygame.init()
 pygame.display.set_caption("moving particles")
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
-fps = 600
+fps = 60
 
 
 
@@ -31,29 +31,27 @@ def createpoints(n):
     return np.concatenate((coords,velocities,colours),axis=1)
 
 def updatepoints(points):
-    points[:,0] = points[:,0] + points[:,2]
-    points[:,1] = points[:,1] + points[:,3]
-    points[:,2] = np.where((points[:,0]>50) & (points[:,0]<850),points[:,2], -points[:,2])
-    points[:,3] = np.where((points[:,1]>50) & (points[:,1]<850),points[:,3], -points[:,3])
+    points[:,0] = points[:,0] + points[:,2] ## updates all the x coords by adding on the x velocities
+    points[:,1] = points[:,1] + points[:,3] ## updates all the y coords by adding on the y velocities
+    points[:,2] = np.where((points[:,0]>50) & (points[:,0]<850),points[:,2], -points[:,2]) ## prevents particles from escaping to the left/right
+    points[:,3] = np.where((points[:,1]>50) & (points[:,1]<850),points[:,3], -points[:,3]) ## prevents particles from escaping to the top/bottom
 
-    coords = points[:,:2]
-    differences = coords.reshape(1000,1,2)-coords
+    coords = points[:,:2] ## gets xy coords of all points
+    differences = coords.reshape(1000,1,2)-coords ## gets the x and y difference between every particle from every other particle
     i = np.arange(1000)
-    differences[i,i] = np.inf
+    differences[i,i] = np.inf ## prevents nearest neighbour being the particle itself
     distances = (differences**2).sum(2) ## gets euclidean distance from every other point
     neighbours = np.argmin(distances,1) ## returns indexes of nearest particle for every particle
-    neighbourdist = (coords - coords[neighbours])
-    neighbourdist = (neighbourdist**2).sum(1)
-    points[:,4] = np.where(neighbourdist[:]<100,1,0)
+    neighbourdist = (coords - coords[neighbours]) ## gets the x and y difference of the rticke and the nearest psrticle to it
+    neighbourdist = (neighbourdist**2).sum(1) ## gets the euclidian distance between a particle and its nearest neighbour
+    points[:,4] = np.where(neighbourdist[:]<100,1,0) ## checks if particle is close enough to its nearest neighbour to be colliding essentially checking is distance is less than or equal to double the radius of the particle
     return points
 
-def wallcollision():
-    pass
 
 def drawpoint(x,y,colour):
-    if colour == 0:
+    if colour == 0: ## this is 0 when a particle is not colliding with another particle
         pygame.draw.circle(screen, GREEN, (x,y), 5)
-    else:
+    else: ## otherwise it will be 1 if it is colliding with another particle
         pygame.draw.circle(screen, RED, (x,y), 5)
 
 
